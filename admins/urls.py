c9340ddab13f129eb9b42c97b3f265c129c1c49c
Tatiana <lexta@ist.ru>
2021-09-05 12:59:58 +0300

    созданы контроллеры для чтения, создания и обновления

--- admins/urls.py
+++ admins/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from admins.views import index
+from admins.views import index, admin_users, admin_users_create, admin_user_update
 
 app_name = 'admins'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('users/', admin_users, name='admin_users'),
+    path('users-create/', admin_users_create, name='admin_users_create'),
+    path('users-update/<int:id>', admin_user_update, name='admin_user_update'),
 ]
--- admins/views.py
+++ admins/views.py
@@ -1,6 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+
+from users.models import User
+from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
 
 
 def index(request):
     context = {'title': 'GeekShop - Admin'}
     return render(request, 'admins/index.html', context)
+
+
+def admin_users(request):
+    context = {'title': 'Geekshop - Пользователи', 'users': User.objects.all()}
+    return render(request, 'admins/admin-users.html', context)
+
+
+def admin_users_create(request):
+    if request.method == 'POST':
+        form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserAdminRegistrationForm()
+    context = {'title': 'Geekshop - Создание пользователя', 'form': form}
+    return render(request, 'admins/admin-users-create.html', context)
+
+
+def admin_user_update(request, id):
+    selected_user = User.objects.get(id=id)
+    if request.method == 'POST':
+        form = UserAdminProfileForm(instance=selected_user, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserAdminProfileForm(instance=selected_user)
+    context = {
+        'title': 'Geekshop - Редактирование пользователя',
+        'selected_user': selected_user,
+        'form': form
+    }
+    return render(request, 'admins/admin-user-update-delete.html', context)
