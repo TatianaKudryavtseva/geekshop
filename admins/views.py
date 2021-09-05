78809fd0566f1e3471e3d5a0a249c35007ec5463
Tatiana <lexta@ist.ru>
2021-09-05 14:20:15 +0300

    реализована защита к админки декоратором @user_passes_test

--- admins/views.py
+++ admins/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.decorators import user_passes_test
 
 from users.models import User
 from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
 
 
+@user_passes_test(lambda u: u.is_staff)
 def index(request):
     context = {'title': 'GeekShop - Admin'}
     return render(request, 'admins/index.html', context)
 
 
+@user_passes_test(lambda u: u.is_staff)
 def admin_users(request):
     context = {'title': 'Geekshop - Пользователи', 'users': User.objects.all()}
     return render(request, 'admins/admin-users.html', context)
 
 
+@user_passes_test(lambda u: u.is_staff)
 def admin_users_create(request):
     if request.method == 'POST':
         form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
@@ -27,6 +31,7 @@ def admin_users_create(request):
     return render(request, 'admins/admin-users-create.html', context)
 
 
+@user_passes_test(lambda u: u.is_staff)
 def admin_user_update(request, id):
     selected_user = User.objects.get(id=id)
     if request.method == 'POST':
@@ -44,6 +49,7 @@ def admin_user_update(request, id):
     return render(request, 'admins/admin-user-update-delete.html', context)
 
 
+@user_passes_test(lambda u: u.is_staff)
 def admin_user_delete(requets, id):
     user = User.objects.get(id=id)
     user.is_active = False
